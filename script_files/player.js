5fc3bdd37ddadbd0cdfa9f3e7ee886be2ad5cce8
Lil Joe <185288979+Lil-Joe-Ras@users.noreply.github.com>
2026-04-28 10:57:33 -0500

    Update player.js

--- script_files/player.js
+++ script_files/player.js
@@ -1,17 +1,46 @@
-let player, camera;
-
-function initPlayer(scene) {
+function initPlayer() {
     player = {
         pos: new BABYLON.Vector3(0, 10, 0),
         vel: new BABYLON.Vector3(0, 0, 0),
-        onGround: false
+        size: new BABYLON.Vector3(0.4, 0.9, 0.4),
+        onGround: false,
+        health: 100
     };
 
     camera = new BABYLON.UniversalCamera("cam", player.pos, scene);
     camera.inertia = 0;
+    camera.inputs.clear();
 }
 
 function updatePlayer() {
-    player.pos.addInPlace(player.vel);
+    const forward = new BABYLON.Vector3(Math.sin(camera.rotation.y), 0, Math.cos(camera.rotation.y));
+    const right = new BABYLON.Vector3(Math.cos(camera.rotation.y), 0, -Math.sin(camera.rotation.y));
+
+    let move = new BABYLON.Vector3(0,0,0);
+
+    if (keys["KeyW"]) move.addInPlace(forward);
+    if (keys["KeyS"]) move.subtractInPlace(forward);
+    if (keys["KeyA"]) move.subtractInPlace(right);
+    if (keys["KeyD"]) move.addInPlace(right);
+
+    if (move.lengthSquared() > 0) {
+        move.normalize();
+        player.vel.x = move.x * 10 * dt;
+        player.vel.z = move.z * 10 * dt;
+    }
+
+    player.vel.y -= 1.6 * dt;
+
+    moveAxis("x");
+    moveAxis("z");
+    moveAxis("y");
+
+    const groundY = getGroundHeight(player.pos.x, player.pos.z);
+    if (player.pos.y <= groundY + player.size.y) {
+        player.pos.y = groundY + player.size.y;
+        player.vel.y = 0;
+        player.onGround = true;
+    }
+
     camera.position.copyFrom(player.pos);
 }
