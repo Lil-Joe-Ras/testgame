a33620a0dbef1239a5227e492a233e3c17e2e0ad
Lil Joe <185288979+Lil-Joe-Ras@users.noreply.github.com>
2026-04-28 10:59:01 -0500

    Update input.js

--- script_files/input.js
+++ script_files/input.js
@@ -1,8 +1,10 @@
-const keys = {};
-
-function initInput(canvas) {
-    window.addEventListener("keydown", e => keys[e.code] = true);
-    window.addEventListener("keyup", e => keys[e.code] = false);
+function initInput(){
+    window.addEventListener("keydown", e => keys[e.code]=true);
+    window.addEventListener("keyup", e => keys[e.code]=false);
 
     canvas.addEventListener("click", () => canvas.requestPointerLock());
 }
+
+window.addEventListener("mousedown", e=>{
+    if(e.button===0) fire();
+});
