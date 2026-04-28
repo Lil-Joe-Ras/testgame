2c6afaf4e8116425a066004192ccdd692d17f334
Lil Joe <185288979+Lil-Joe-Ras@users.noreply.github.com>
2026-04-28 10:56:55 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,27 +1,22 @@
 const scripts = [
+  "script_files/constant.js",
   "script_files/player.js",
   "script_files/world.js",
   "script_files/weapons.js",
   "script_files/input.js",
   "script_files/effects.js",
   "script_files/ui.js",
+  "script_files/render/collision.js",
   "script_files/render/render.js",
   "script.js"
 ];
 
-let loaded = 0;
-
-function loadNext() {
-  if (loaded >= scripts.length) return;
-
+let i = 0;
+function load() {
+  if (i >= scripts.length) return;
   const s = document.createElement("script");
-  s.src = scripts[loaded];
-  s.onload = () => {
-    loaded++;
-    loadNext();
-  };
-
+  s.src = scripts[i++];
+  s.onload = load;
   document.body.appendChild(s);
 }
-
-loadNext();
+load();
